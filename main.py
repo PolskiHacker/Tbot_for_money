2da7f3e6255403b550e73ebabb412baa4c4e2402
sasha <sashajuravsky@gmail.com>
2024-08-08 01:49:21 +0300

    timer edit

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ from config_reader import config
 from handlers import basic_handlers, work_handlers
 from parsing_bd.DB_func import check
 
-
 logging.basicConfig(level=logging.INFO)
 
 
 async def timer():
     while True:
         now = datetime.now()
-        # next_day = (now +timedelta(day=1)).replace(hour=0, minute=0, second=0, microsecond=0)
-        # wait_time=(next_day-now).total_seconds()
-        next_minute = (now + timedelta(minutes=1)).replace(second=0, microsecond=0)
-        wait_time = (next_minute - now).total_seconds()
+        next_day = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+        wait_time = (next_day - now).total_seconds()
         check()
         await asyncio.sleep(wait_time)
 
